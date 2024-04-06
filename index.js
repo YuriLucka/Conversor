f1148d3d7650ff9d3a7a9950eafd15da47671443
Yuri Lucka Carriel Rodrigues <108015995+YuriLucka@users.noreply.github.com>
2024-04-06 10:42:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let imagem = document.querySelector(".image")
+
+imagem.addEventListener('click', obrigado)
+
+function obrigado(){
+    alert("Parabéns, você descobriu um Easter egg, pena que não vai te ajudar na prova. 😎\n\nBoa sorte na prova e saiba que o melhor professor da Facul sempre será o Chico. 🔺")
+}
